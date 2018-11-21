c923a4eee40b8567b82e0830aee31bd46edc6c28
ricofung <ricofungcn@icloud.com>
2018-11-21 09:50:28 +0800

    "No MyBatis mapper was found in '[com]' package. " 警告

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -5,13 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
-// 默认
+// 默认（只能扫描project内的bean）
 //@SpringBootApplication
-// 需要客制化security时用
+// 自定义（可扫描project外的bean）
 @ComponentScan(basePackages={"com","chok.common"})
 // exclude表示自动配置时排除特定配置（使用CommonsMultipartFile实现多文件上传）
 @EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class })
@@ -26,11 +24,5 @@ public class Application extends SpringBootServletInitializer
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
 	{
 		return application.sources(Application.class);
-	}
-	
-	@Bean
-	public RestTemplate restTemplate()
-	{
-		return new RestTemplate();
 	}
 }
