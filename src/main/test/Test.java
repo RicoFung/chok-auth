9c41909abb26a5c47d7aed8f66441aabb56ea347
ricofung <ricofungcn@icloud.com>
2019-11-01 13:33:45 +0800

    集成redisson访问redis集群

--- src/main/test/Test.java
+++ src/main/test/Test.java
@@ -1,13 +1,42 @@
 import org.junit.runner.RunWith;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.client.codec.StringCodec;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.BoundHashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.Application;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
+@SpringBootTest(classes = Application.class,
+properties = {
+//        "spring.redis.redisson.config=classpath:redisson-single.yml"/*,
+        "spring.redis.redisson.config=classpath:redisson-cluster.yml"/*,
+        "spring.redis.timeout=10000"*/
+    })
 public class Test
 {
+    @Autowired
+    private RedissonClient redisson;
+    
+    @Autowired
+    private RedisTemplate<String, String> template;
+    
+	@org.junit.Test
+	public void testApp()
+	{
+//		redisson.getKeys().flushall();
+
+//		RMap<String, String> m = redisson.getMap("test1", StringCodec.INSTANCE);
+//		m.put("1", "12");
+
+		BoundHashOperations<String, String, String> hash = template.boundHashOps("test1");
+		String t = hash.get("1");
+		System.out.println("[TEST1] <= "+t.equals("12"));
+	}
 //	@Autowired
 //	LoadBalancerClient	loadBalancerClient;
 //	@Autowired
